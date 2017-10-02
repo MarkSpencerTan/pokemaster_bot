b393206454177e69ada3037faa964c547816175c
Mark Tan <macbonkaynel@gmail.com>
2017-10-02 00:40:05 -0700

    update settings

--- settings.py
+++ settings.py
@@ -1,12 +1,9 @@
-import requests
+import os
 
-r = requests.get("https://api.heroku.com/apps/pokemasterbot/config-vars").json()
-print(r)
-
-CLIENT_ID = r.get("CLIENT_ID")
-CLIENT_SECRET = r.get("CLIENT_SECRET")
-BOT_TOKEN = r.get("BOT_TOKEN")
-MONGO_URI = r.get("MONGO_URI")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+MONGO_URI = os.environ.get("MONGO_URI")
 
 SHINY_RATE = .02
 BOT_ADMIN = ["Marko Spencer#0713"]
