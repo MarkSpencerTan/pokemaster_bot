61d9bff104d6e658ae59392010d02dc75fad7ac3
Mark Tan <macbonkaynel@gmail.com>
2017-10-02 00:30:47 -0700

    udpaet

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 import requests
 
 r = requests.get("https://api.heroku.com/apps/pokemasterbot/config-vars").json()
-print r
+print(r)
 
-CLIENT_ID = r["CLIENT_ID"]
-CLIENT_SECRET = r["CLIENT_SECRET"]
-BOT_TOKEN = r["BOT_TOKEN"]
-MONGO_URI = r["MONGO_URI"]
+CLIENT_ID = r.get("CLIENT_ID")
+CLIENT_SECRET = r.get("CLIENT_SECRET")
+BOT_TOKEN = r.get("BOT_TOKEN")
+MONGO_URI = r.get("MONGO_URI")
 
 SHINY_RATE = .02
 BOT_ADMIN = ["Marko Spencer#0713"]
